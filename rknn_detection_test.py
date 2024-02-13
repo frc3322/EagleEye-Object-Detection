fc82939d3b5447e43611374480441a3e02aabdaf
DarkEden-coding <darkedenc9@gmail.com>
2024-02-13 12:28:52 -0500

    update rknn_detection_test.py

--- rknn_detection_test.py
+++ rknn_detection_test.py
@@ -1,113 +1,27 @@
-import cv2
-import numpy as np
-import platform
 from rknnlite.api import RKNNLite
-from time import time
-
-# device tree for RK356x/RK3588
-DEVICE_COMPATIBLE_NODE = '/proc/device-tree/compatible'
-
-labels = {0: "notes"}
-
-RK3566_RK3568_RKNN_MODEL = 'resnet18_for_rk3566_rk3568.rknn'
-RK3588_RKNN_MODEL = 'color_model.rknn'
-RK3562_RKNN_MODEL = 'resnet18_for_rk3562.rknn'
-
-image_path = './test-note.jpg'
 
 
-def get_host():
-    # get platform and device type
-    system = platform.system()
-    machine = platform.machine()
-    os_machine = system + '-' + machine
-    if os_machine == 'Linux-aarch64':
-        try:
-            with open(DEVICE_COMPATIBLE_NODE) as f:
-                device_compatible_str = f.read()
-                if 'rk3588' in device_compatible_str:
-                    host = 'RK3588'
-                elif 'rk3562' in device_compatible_str:
-                    host = 'RK3562'
-                else:
-                    host = 'RK3566_RK3568'
-        except IOError:
-            print('Read device node {} failed.'.format(DEVICE_COMPATIBLE_NODE))
-            exit(-1)
-    else:
-        host = os_machine
-    return host
+class RKNN_model_container:
+    def __init__(self, model_path, target=None, device_id=None) -> None:
+        rknn = RKNNLite()
 
+        rknn.load_rknn(model_path)
 
-def show_top5(result):
-    output = result[0].reshape(-1)
-    # Softmax
-    output = np.exp(output) / np.sum(np.exp(output))
-    # Get the indices of the top 5 largest values
-    output_sorted_indices = np.argsort(output)[::-1][:5]
-    print('output_sorted_indices: ' + str(np.argsort(output)))
-    top5_str = 'resnet18\n-----TOP 5-----\n'
-    for i, index in enumerate(output_sorted_indices):
-        value = output[index]
-        if value > 0:
-            topi = '[{:>3d}] score:{:.6f} class:"{}"\n'.format(index, value, "note")
+        print('--> Init runtime environment')
+        ret = rknn.init_runtime()
+        if ret != 0:
+            print('Init runtime environment failed')
+            exit(ret)
+        print('done')
+
+        self.rknn = rknn
+
+    def run(self, inputs):
+        if isinstance(inputs, list) or isinstance(inputs, tuple):
+            pass
         else:
-            topi = '-1: 0.0\n'
-        top5_str += topi
-    print(top5_str)
+            inputs = [inputs]
 
+        result = self.rknn.inference(inputs=inputs)
 
-if __name__ == '__main__':
-
-    # Get device information
-    host_name = get_host()
-    if host_name == 'RK3566_RK3568':
-        rknn_model = RK3566_RK3568_RKNN_MODEL
-    elif host_name == 'RK3562':
-        rknn_model = RK3562_RKNN_MODEL
-    elif host_name == 'RK3588':
-        rknn_model = RK3588_RKNN_MODEL
-    else:
-        print("This demo cannot run on the current platform: {}".format(host_name))
-        exit(-1)
-
-    rknn_lite = RKNNLite()
-
-    # Load RKNN model
-    print('--> Load RKNN model')
-    ret = rknn_lite.load_rknn(rknn_model)
-    if ret != 0:
-        print('Load RKNN model failed')
-        exit(ret)
-    print('done')
-
-    ori_img = cv2.imread(image_path)
-    img = cv2.cvtColor(ori_img, cv2.COLOR_BGR2RGB)
-    img = np.expand_dims(img, 0)
-
-    # Init runtime environment
-    print('--> Init runtime environment')
-    # run on RK356x/RK3588 with Debian OS, do not need specify target.
-    if host_name == 'RK3588':
-        # For RK3588, specify which NPU core the model runs on through the core_mask parameter.
-        ret = rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_0)
-    else:
-        ret = rknn_lite.init_runtime()
-    if ret != 0:
-        print('Init runtime environment failed')
-        exit(ret)
-    print('done')
-
-    # Inference
-    print('--> Running model')
-    start_time = time()
-    outputs = rknn_lite.inference(inputs=[img])
-
-    # print inference time in ms
-    print('inference time: {} ms'.format((time() - start_time) * 1000))
-
-    # Show the classification results
-    show_top5(outputs)
-    print('done')
-
-    rknn_lite.release()
+        return result
