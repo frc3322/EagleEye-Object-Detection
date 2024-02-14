2afbe529fb227a270ba3b49ef3bdae0014e53174
DarkEden-coding <darkedenc9@gmail.com>
2024-02-13 19:45:03 -0500

    na

--- onnx_export.py
+++ onnx_export.py
@@ -7,5 +7,5 @@ print("Model loaded.")
 
 # Export the model
 print("Exporting model...")
-model.export(format='onnx', imgsz=640, opset=13, half=True)
+model.export(format='onnx', imgsz=640, opset=19, half=True, int8=True, simplify=True)
 print("Model exported.")
