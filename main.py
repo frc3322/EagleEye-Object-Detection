radians fix

main.py
@@ -34,7 +34,7 @@ def calculate_local_note_position(x_angle, y_angle):
     :return: the position of the note in the local coordinate system
     """
     # soh cah toa
-    x_position = np.tan(90 + y_angle) * CameraConstants.camera_height
+    x_position = np.tan(np.radians(90 + y_angle)) * CameraConstants.camera_height
     print(f"process angle: {90 + y_angle}")
 
     return rotate2d((x_position, 0), 0)