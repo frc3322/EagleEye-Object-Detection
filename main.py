1497402b2213a559dd7bdd32b09d57dc9b3867ec
DarkEden-coding <darkedenc9@gmail.com>
2024-02-17 11:00:21 -0500

    na

--- main.py
+++ main.py
@@ -180,14 +180,14 @@ def calculation_thread(camera_data):
                     detection_data[camera_data['name']] = note_data
                     print(f"detection_data: {detection_data}")
 
-                    print(f"Total frame time: {(time() - start_time) * 1000}ms\n")
-                    print(f"Est fps: {1 / (time() - start_time)}\n")
-
-                    print("-" * 60)
-
             if DisplayConstants.show_output:
                 # Display the resulting frame
                 cv2.imshow('frame', frame)
+
+                print(f"Total frame time: {(time() - start_time) * 1000}ms\n")
+                print(f"Est fps: {1 / (time() - start_time)}\n")
+
+                print("-" * 60)
 
     except KeyboardInterrupt:
         print("Keyboard interrupt")
