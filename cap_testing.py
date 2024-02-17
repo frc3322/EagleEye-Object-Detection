3f50c5663319eedce645a6b2b2dce63b15dad85c
DarkEden-coding <darkedenc9@gmail.com>
2024-02-17 11:47:23 -0500

    na

--- cap_testing.py
+++ cap_testing.py
@@ -1,13 +1,12 @@
 import cv2
 from time import time, sleep
 
-
-# code that reads from camera 0 and displays the video feed
-cap = cv2.VideoCapture(2)
-
 start = time()
 sleep(1)
 print(f"Time taken in ms: {(time() - start) * 1000}ms")
+
+# code that reads from camera 0 and displays the video feed
+cap = cv2.VideoCapture(2)
 
 while True:
     ret, frame = cap.read()
