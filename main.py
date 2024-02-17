0f500121362d47cb7ac09da762abf57669b2be4c
DarkEden-coding <darkedenc9@gmail.com>
2024-02-17 14:17:55 -0500

    na

--- main.py
+++ main.py
@@ -131,7 +131,7 @@ def calculation_thread(camera_data):
         while running:
             start_time = time()
             # Capture frame-by-frame
-            print(f"Getting frame for {camera_data}")
+            print(f"Getting frame for {camera_images}")
             frame = camera_images[camera_data['name']]
 
             if DisplayConstants.show_output:
