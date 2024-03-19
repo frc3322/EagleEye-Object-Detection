df110de85b53cd972ab7c27349451bccf00a7d1c
DarkEden-coding <darkedenc9@gmail.com>
2024-03-19 00:17:09 -0400

    include setting in constants.py

--- constants.py
+++ constants.py
@@ -10,7 +10,9 @@ class ObjectDetectionConstants:
     model_path = "model_creation/color_model.onnx"
     confidence_threshold = 0.65
 
-    note_combined_threshold = 10  # the threshold for combining notes in the same position
+    note_combined_threshold = (
+        10  # the threshold for combining notes in the same position
+    )
 
 
 class DisplayConstants:
@@ -21,15 +23,15 @@ class DisplayConstants:
 class CameraConstants:
     camera_list = [
         {
-            'name': 'front',
-            'camera_id': 0,  # the id of the camera, 0 for the default camera, you can change it if you have multiple
-            'camera_width_angle': 70,  # the max view angle in degrees
-            'camera_height_angle': 38,  # the max view angle in degrees
-
-            'camera_offset_pos': np.array([0, 0, 73.34]),  # the offset of the camera from the center of the robot
+            "name": "front",
+            "camera_id": 0,  # the id of the camera, 0 for the default camera, you can change it if you have multiple
+            "camera_width_angle": 70,  # the max view angle in degrees
+            "camera_height_angle": 38,  # the max view angle in degrees
+            "camera_offset_pos": np.array(
+                [0, 0, 73.34]
+            ),  # the offset of the camera from the center of the robot
             # in cm
-
-            'camera_v_angle': 0,  # the angle of the camera from directly forward, positive is up
-            'camera_h_angle': 0  # the angle of the camera from directly forward, positive is right
+            "camera_v_angle": 0,  # the angle of the camera from directly forward, positive is up
+            "camera_h_angle": 0,  # the angle of the camera from directly forward, positive is right
         }
     ]
