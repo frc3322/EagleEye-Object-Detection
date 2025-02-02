b286fd2abaf7f93795ea456b17b90b5287906aa9
DarkEden-coding <darkedenc9@gmail.com>
2025-02-01 22:15:01 -0500

    update requirements.txt

--- src/format_conversion/manual_convert.py
+++ src/format_conversion/manual_convert.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load YOLO model
-model = YOLO(r'/Users/darkeden/PycharmProjects/FIRST-Object-Detection/src/models/GamePiece.pt')  # Replace with your model path
+model = YOLO(r'E:\Ceph-Mirror\Python-Files\Projects\FIRST-Note-Detection\src\models\GamePiece.pt')  # Replace with your model path
 
 model_path = model.export(format='edgetpu')
 
