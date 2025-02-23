935e2618882cadb89f88b2361dd4df8790277c54
DarkEden-coding <darkedenc9@gmail.com>
2025-02-22 19:50:10 -0500

    Update qodana.yaml

--- qodana_setup.sh
+++ qodana_setup.sh
@@ -6,9 +6,6 @@ set -e
 # Create virtual environment
 python3 -m venv /data/cache/venv
 
-# Activate virtual environment
-source /data/cache/venv/bin/activate
-
 # Install dependencies
 pip install -r requirements.txt
 
